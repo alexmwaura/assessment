e6cf93f62e1fd82e71596aa0666103fbd1932251
alexmwaura <alexmwaura43@gmail.com>
2020-11-07 17:36:55 +0300

    modify test

--- src/test/java/MissingIntegerTest.java
+++ src/test/java/MissingIntegerTest.java
@@ -6,15 +6,9 @@ import static org.junit.Assert.*;
 
 public class MissingIntegerTest {
     @Test
-    public void getSize_returnLengthOfArray_4(){
-        int[] arrayIntegers = {1,2,3,4,5};
+    public void getMissingInteger_returnMissingInteger_6(){
+        int[] arrayIntegers = {1,2,3,4,5,7};
         MissingInteger missingInteger = new MissingInteger(arrayIntegers);
-        assertEquals(5,missingInteger.getLengthArrayIntegers());
-    }
-    @Test
-    public void getMissingInteger_returnMissingInteger_2(){
-        int[] arrayIntegers = {1,3,4,5};
-        MissingInteger missingInteger = new MissingInteger(arrayIntegers);
-        assertEquals(2,missingInteger.getMissingNumber());
+        assertEquals(6,missingInteger.getMissingNumber());
     }
 }
